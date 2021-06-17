4399cdf1ca12ee3e40e4b41e2804f301ecc9f7ee
phlinhng <phlinhng@outlook.com>
2021-06-18 05:56:35 +0800

    Update warp-google.sh

--- warp-google.sh
+++ warp-google.sh
@@ -25,10 +25,10 @@ curl -s https://www.gstatic.com/ipranges/goog.json | jq ".prefixes | .[] | .ipv4
 for googip4 in `cat /tmp/goog_ip4s`; do echo "ip route add $googip4 via $gateway dev $interface" >> /root/wg-up.sh; done
 for googip4 in `cat /tmp/goog_ip4s`; do echo "ip route del $googip4 via $gateway dev $interface" >> /root/wg-down.sh; done
 
-# get cloudflare's ipv6 range
+# get cloudflare's ipv4 range
 #curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ip4s
-for cfip4 in `cat /tmp/cf_ip4s`; do echo "ip route add $cfip4 via $gateway dev $interface" >> /root/wg-up.sh; done
-for cfip4 in `cat /tmp/cf_ip4s`; do echo "ip route del $cfip4 via $gateway dev $interface" >> /root/wg-down.sh; done
+#for cfip4 in `cat /tmp/cf_ip4s`; do echo "ip route add $cfip4 via $gateway dev $interface" >> /root/wg-up.sh; done
+#for cfip4 in `cat /tmp/cf_ip4s`; do echo "ip route del $cfip4 via $gateway dev $interface" >> /root/wg-down.sh; done
 
 # disable ipv6 dns records
 #echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
